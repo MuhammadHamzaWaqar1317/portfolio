1392aafece102a7a9d9c5965b08d2cee1e61910d
MuhammadHamzaWaqar1317 <waqarhamza2003@gmail.com>
2024-08-25 23:15:30 +0500

    feat: added ui components sparkles, cover, spotlight

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from "next/image";
-
+import Hero from "@/components/Hero";
+import '../app/globals.css'
 export default function Home() {
   return (
-   <></>
+   <>
+   <Hero/>
+   </>
   );
 }
